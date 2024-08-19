51569e58361c07f05b31a0b5e9f576316e9320c2
Quin Schaller <qaschaller@gmail.com>
2024-08-19 09:44:16 -0500

    project

---
+++ All About Me/QuinView.swift
@@ -0,0 +1,18 @@
+//
+//  QuinView.swift
+//  All About Me
+//
+//  Created by Quin Schaller on 8/19/24.
+//
+
+import SwiftUI
+
+struct QuinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuinView()
+}
